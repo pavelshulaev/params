2bbbc8024669d425bd43458c8f5e7d7005f54252
pavel <rover.webdev@gmail.com>
2017-10-31 22:32:05 +0300

    add support

---
+++ lib/support.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 31.10.2017
+ * Time: 20:25
+ *
+ * @author Pavel Shulaev (https://rover-it.me)
+ */
+
+namespace Rover\Params;
+
+use Bitrix\Main\ArgumentNullException;
+use Rover\Params\Engine\Cache;
+use Rover\Params\Engine\Core;
+
+class Support extends Core
+{
+    /**
+     * @var string
+     */
+    protected static $moduleName = 'support';
+
+    /**
+     * @param array $params
+     * @return null
+     * @author Pavel Shulaev (https://rover-it.me)
+     */
+    public static function getCategory(array $params = array())
+    {
+        return self::getDictionaryByType('C', $params);
+    }
+
+    /**
+     * @param array $params
+     * @return null
+     * @author Pavel Shulaev (https://rover-it.me)
+     */
+    public static function getCriticality(array $params = array())
+    {
+        return self::getDictionaryByType('K', $params);
+    }
+
+    /**
+     * @param       $type
+     * @param array $params
+     * @return null
+     * @throws ArgumentNullException
+     * @author Pavel Shulaev (https://rover-it.me)
+     */
+    public static function getDictionaryByType($type, array $params = array())
+    {
+        self::checkModule();
+
+        $type = trim($type);
+        if (!strlen($type))
+            throw new ArgumentNullException('dictionary type');
+
+        $params['filter']['TYPE'] = $type;
+
+        if (empty($params['order']))
+            $params['order'] = array("s_c_sort" => 'asc');
+
+        $params     = self::prepareParams($params);
+        $cacheKey   = Cache::getKey(__METHOD__, serialize($params));
+
+        if((false === (Cache::check($cacheKey))) || $params['reload']) {
+
+            $result     = self::getStartResult($params['empty']);
+
+            $dbelements = \CTicketDictionary::GetList(
+                $by = key($params['order']),
+                $order = $params['order'][$by],
+                $params['filter'],
+                $is_filtered
+            );
+
+            $elements   = array();
+
+            while ($group = $dbelements->Fetch())
+                $elements[] = $group;
+
+            $result = self::prepareResult($elements, $params['template'], $result);
+
+            Cache::set($cacheKey, $result);
+        }
+
+        return Cache::get($cacheKey);
+    }
+
+    /**
+     * @return null
+     * @author Pavel Shulaev (https://rover-it.me)
+     */
+    public static function getSla()
+    {
+        self::checkModule();
+
+        if (empty($params['order']))
+            $params['order'] = array("PRIORITY" => 'DESC');
+
+        $params     = self::prepareParams($params);
+        $cacheKey   = Cache::getKey(__METHOD__, serialize($params));
+
+        if((false === (Cache::check($cacheKey))) || $params['reload']) {
+
+            $result     = self::getStartResult($params['empty']);
+            $dbelements = \CTicketSLA::GetList($params['order'], $params['filter']);
+            $elements   = array();
+
+            while ($group = $dbelements->Fetch())
+                $elements[] = $group;
+
+            $result = self::prepareResult($elements, $params['template'], $result);
+
+            Cache::set($cacheKey, $result);
+        }
+
+        return Cache::get($cacheKey);
+    }
+}
