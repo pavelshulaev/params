3fd77d88074c07528b5bbbe00bda0c5b9fdc1cbd
pavel <rover.webdev@gmail.com>
2018-10-30 16:10:48 +0300

    add sale methods

--- lib/sale.php
+++ lib/sale.php
@@ -25,12 +25,15 @@ class Sale extends Core
 	 */
 	protected static $moduleName = 'sale';
 
-	/**
-	 * @param array $params
-	 * @return array|null
-	 * @throws \Bitrix\Main\SystemException
-	 * @author Pavel Shulaev (https://rover-it.me)
-	 */
+    /**
+     * @param array $params
+     * @return array|null
+     * @throws \Bitrix\Main\ArgumentException
+     * @throws \Bitrix\Main\ArgumentOutOfRangeException
+     * @throws \Bitrix\Main\LoaderException
+     * @throws \Bitrix\Main\SystemException
+     * @author Pavel Shulaev (https://rover-it.me)
+     */
 	public static function getOrderStatuses(array $params = array())
 	{
 		self::checkModule();
@@ -40,8 +43,8 @@ class Sale extends Core
 		$params['class']    = 'Bitrix\Sale\Internals\StatusLangTable';
 		$params['method']   = 'getList';
 
-		if (!isset($params['filter']))
-			$params['filter'] = array('=LID' => $lid);
+		if (!isset($params['filter']['=LID']))
+			$params['filter']['=LID'] = $lid;
 
 		if (!isset($params['order']))
 			$params['order'] = array('STATUS.SORT' => 'ASC');
@@ -55,6 +58,10 @@ class Sale extends Core
     /**
      * @param array $params
      * @return array|null
+     * @throws \Bitrix\Main\ArgumentException
+     * @throws \Bitrix\Main\ArgumentOutOfRangeException
+     * @throws \Bitrix\Main\LoaderException
+     * @throws \Bitrix\Main\SystemException
      * @author Pavel Shulaev (https://rover-it.me)
      */
 	public static function getPaySystems(array $params = array())
@@ -69,6 +76,59 @@ class Sale extends Core
 
         if (!isset($params['template']))
             $params['template'] = array('{PAY_SYSTEM_ID}' => '{NAME} [{PAY_SYSTEM_ID}]');
+
+        return self::prepare($params);
+    }
+
+
+    /**
+     * @param array $params
+     * @return array|null
+     * @throws \Bitrix\Main\ArgumentException
+     * @throws \Bitrix\Main\ArgumentOutOfRangeException
+     * @throws \Bitrix\Main\LoaderException
+     * @throws \Bitrix\Main\SystemException
+     * @author Pavel Shulaev (https://rover-it.me)
+     */
+	public static function getDeliveryServices(array $params = array())
+    {
+        self::checkModule();
+
+        $params['class']    = 'Bitrix\Sale\Delivery\Services\Table';
+        $params['method']   = 'getList';
+
+        if (!isset($params['order']))
+            $params['order'] = array('NAME' => 'ASC');
+
+        if (!isset($params['template']))
+            $params['template'] = array('{ID}' => '{NAME} [{ID}]');
+
+        return self::prepare($params);
+    }
+
+    /**
+     * @param array $params
+     * @return array|null
+     * @throws \Bitrix\Main\ArgumentException
+     * @throws \Bitrix\Main\ArgumentOutOfRangeException
+     * @throws \Bitrix\Main\LoaderException
+     * @throws \Bitrix\Main\SystemException
+     * @author Pavel Shulaev (https://rover-it.me)
+     */
+    public static function getOrderProperties(array $params = array())
+    {
+        self::checkModule();
+
+        $params['class']    = 'Bitrix\Sale\Internals\OrderPropsTable';
+        $params['method']   = 'getList';
+
+        if (!isset($params['order']))
+            $params['order'] = array('NAME' => 'ASC');
+
+        if (!isset($params['template'])) {
+            $params['template'] = array('{ID}' => '{NAME} [{PERSON_TYPE_NAME}]');
+            $params['select']['PERSON_TYPE_NAME'] = 'PERSON_TYPE.NAME';
+        }
 
         return self::prepare($params);
     }
